b5d96d864c2ba79a0237f0ae8082396a15859224
James <james@jamesalves.com.br>
2018-06-22 20:48:55 -0300

    Capitulo 1

---
+++ CAP1.R
@@ -0,0 +1,79 @@
+library(dplyr)
+library(tidytext)
+library(janeaustenr)
+library(stringr)
+library(ggplot2)
+
+texto <- c("É um fato conhecido de todos que um leitor",
+           "se distrairá com o conteúdo de texto legível",
+           "de uma página quando estiver examinando sua diagramação.",
+           "A vantagem de usar Lorem Ipsum é que ele tem uma distribuição")
+texto
+tx_df <- data_frame(line = 1:4, text = texto)
+tx_df
+tx_df %>%
+  unnest_tokens(word, text)
+
+livro_orignal <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linha = row_number(),
+         capitulo = cumsum(str_detect(text, regex("^CHAPTER [\\divxlc]", 
+                                                  ignore_case = TRUE)))) %>%
+  ungroup()
+
+livro_orignal
+
+livro_tidy <- livro_orignal %>%
+  unnest_tokens(word, text)
+
+livro_tidy
+# dados de palavras paradas
+data("stop_words")
+
+# remover palavras
+livro_tidy <- livro_tidy %>%
+  anti_join(stop_words)
+
+# contagem de paravras
+livro_tidy %>%
+  count(word, sort = TRUE)%>%
+  filter(n > 600)%>%
+  mutate(word = reorder(word, n))%>%
+  ggplot(aes(word, n)) + 
+           geom_col() +
+           xlab(NULL) +
+           coord_flip()
+
+  
+library(gutenbergr)
+
+livros_hg <- gutenberg_download(c(35,36,5230, 159))
+tidy_hg <- livros_hg %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+tidy_hg %>%
+  count(word, sort = TRUE)
+
+livros_bro <- gutenberg_download(c(1260,768,969,91,82, 767))
+tidy_bro <- livros_bro %>%
+  unnest_tokens(word, text)%>%
+  anti_join(stop_words)
+
+tidy_bro%>%
+  count(word, sort = TRUE)
+
+library(tidyr)
+frequencia <- bind_rows(mutate(tidy_bro, author = "Bronte Sisters"),
+                        mutate(tidy_hg,  author = "H.G. Hells"),
+                        mutate(livro_tidy, author ="Jane Austen")) %>%
+  mutate(word = str_extract(word, "[a-z']+")) %>%
+  count(author, word) %>%
+  group_by(author) %>%
+  mutate(proportion = n/sum(n)) %>%
+  select(-n) %>%
+  spread(author, proportion) %>%
+  gather(author, proportion,"Bronte Sisters":"H.G. Hells" )
+
+ggplot(frequencia, aes(x= proportion, y = 'Jane Austen', 
+                       color = abs('Jane Austen' - proportion)))
